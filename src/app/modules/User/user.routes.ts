6ed1c3258b481b3abf9e20c777a2f915d06269b2
SharminMily <sharminakther5599@gmail.com>
2025-09-10 00:29:20 +0600

    file added for image

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import { UserController } from "./user.controller"
+import { fileUploder } from "../../../helpers/fileUploder"
 
 const router = express.Router()
 
 router.get('/:id', UserController.getByIdFromDB)
-router.post('/', UserController.createUser)
+router.post('/',
+    fileUploder.upload.single('file'),
+     UserController.createUser)
 router.patch('/:id', UserController.updateIdFromDB)
 router.delete('/:id', UserController.deleteFromDB)
 router.get('/', UserController.getAllUserFromDB)
